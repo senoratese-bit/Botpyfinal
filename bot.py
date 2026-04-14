fa9d53fc1751e47db3504885d639a82ac416d95e
senoratese-bit <senoratese@gmail.com>
2026-04-14 19:45:07 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "OK"
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    return "Webhook OK"
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
